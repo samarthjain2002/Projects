44faf5681d0b3a9ad17cbbd8a6d0f37da761fc10
Samarth Jain <jsamarth84@gmail.com>
2025-02-04 21:48:44 +0530

    built a word counter

--- Python Projects/Django/Python Backend Web Development Course (with Django)/myproject/myapp/views.py
+++ Python Projects/Django/Python Backend Web Development Course (with Django)/myproject/myapp/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
+def index1(request):
     name = "Sam"
     return render(request, 'index1.html', {'name': name})
+
+def counter(request):
+    text = request.POST['text']
+    number = len(text.split())
+    return render(request, 'counter.html', {"number": number})
