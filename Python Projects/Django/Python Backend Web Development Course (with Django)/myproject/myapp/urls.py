41785d1a190be041de8becbec1f42cc99b57b9f0
Samarth Jain <jsamarth84@gmail.com>
2025-02-05 23:11:49 +0530

    login and logout

--- Python Projects/Django/Python Backend Web Development Course (with Django)/myproject/myapp/urls.py
+++ Python Projects/Django/Python Backend Web Development Course (with Django)/myproject/myapp/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('index1', views.index1, name = "index1"),
     path('counter', views.counter, name = "counter"),
     path('register', views.register, name = "register"),
+    path('login', views.login, name = "login"),
+    path('logout', views.logout, name = "logout"),
 ]
--- Python Projects/Django/Python Backend Web Development Course (with Django)/myproject/myapp/views.py
+++ Python Projects/Django/Python Backend Web Development Course (with Django)/myproject/myapp/views.py
@@ -55,3 +55,23 @@ def register(request):
             return redirect("register")
     else:
         return render(request, "register.html")
+    
+def login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = auth.authenticate(username = username, password = password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, "Credentials Invalid")
+            return redirect("login")
+    else:
+        return render(request, "login.html")
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
