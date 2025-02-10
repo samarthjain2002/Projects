a3fa5d2b7f39b57e9c88948220c0f21de7adc6ac
Samarth Jain <jsamarth84@gmail.com>
2025-02-10 17:58:46 +0530

    Added a feature to have conversation with the seller

--- Python Projects/Django/Online Marketplace using Python and Django/Puddle/puddle/puddle/urls.py
+++ Python Projects/Django/Online Marketplace using Python and Django/Puddle/puddle/puddle/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('items/', include('item.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
+    path('conversation/', include('conversation.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
