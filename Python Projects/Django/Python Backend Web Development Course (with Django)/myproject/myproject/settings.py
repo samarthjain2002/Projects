526b2d1029548f7d5e7e1a96d3924a69a2e2d834
Samarth Jain <jsamarth84@gmail.com>
2025-02-21 19:11:23 +0530

    Changes

--- Python Projects/Django/Python Backend Web Development Course (with Django)/myproject/myproject/settings.py
+++ Python Projects/Django/Python Backend Web Development Course (with Django)/myproject/myproject/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-38j^ke5l39kw%wajx+r00k*--et()_)n7m60ru17ez1^+kn)pt"
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,10 +83,10 @@ WSGI_APPLICATION = "myproject.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "myproject",
-        "USER": "postgres",
-        "PASSWORD": "ArsenalTheBottlers",
-        "HOST": "localhost", 
+        "NAME": os.getenv("name"),
+        "USER": os.getenv("user"),
+        "PASSWORD": os.getenv("dbpassword"),
+        "HOST": os.getenv("dbhost"),
     }
 }
 
